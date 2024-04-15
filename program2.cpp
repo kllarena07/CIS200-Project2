1d36b042c28eacbc0f21d259ff000a32265d24ce
Fated2be <alexcheayto@gmail.com>
2024-04-15 17:14:34 -0400

    feat: Added variables & formatting

--- program2.cpp
+++ program2.cpp
@@ -10,60 +10,91 @@ Purpose: Project Two - Program Two | CPU
 
 ****************/
 
-
 #include <fstream>
 #include <iostream>
 using std::cout;
+using std::endl;
 using std::fstream;
 using std::ios;
 
-struct Job {
+struct Job
+{
   char job_type;
   unsigned int arrival_time;
   unsigned int processing_time;
   unsigned int idle_time;
 };
 
-struct Node {
+struct Node
+{
   struct Job data;
-  Node* next;
-  Node* prev;
+  Node *next;
+  Node *prev;
 };
 
-struct CPU {
+struct CPU
+{
   struct Job current_job;
   unsigned int idle_time;
   unsigned int busy_time;
   unsigned int total_time;
 };
 
-void queue_push_back(Node*& queue_head, Node* data) {
-  Node* current = queue_head;
+void queue_push_back(Node *&queue_head, Node *data)
+{
+  Node *current = queue_head;
 
-  while (current != nullptr) {
+  while (current != nullptr)
+  {
     current = current->next;
   }
   current = data;
 }
 
-int main() {
+int main()
+{
+  // File stuff
   fstream file;
   file.open("jobs.dat", ios::out);
 
   fstream logFile("log.txt", ios::out);
 
-  if (!file) {
-    cout << "Error opening file\n";
+  if (!file)
+  {
+    cout << "Error opening job file\n";
     return 1;
   }
+
+  if (!logFile)
+  {
+    cout << "Error opening log file\n";
+    return 1;
+  }
+
+  // Needed values
+  const int numCPUs = 1;
+  int arrivedAJobs = 0;
+  int arrivedBJobs = 0;
+  int arrivedCJobs = 0;
+  int arrivedDJobs = 0;
+  int completedAJobs = 0;
+  int completedBJobs = 0;
+  int completedCJobs = 0;
+  int completedDJobs = 0;
+  int totalQueueTime = 0;
+  int totalCPUIdleTime = 0;
+  int totalCPUProcessTime = 0;
+  int totalQueueSize = 0;
+  int highestQueueAmount = 0;
 
   // Create file queue
   Job file_queue[7000];
 
   struct Job blank_job = {'_', 0, 0, 0};
   size_t index = 0;
-  while (!file.eof()) {
-    file.read(reinterpret_cast<char*>(&blank_job), sizeof(Job));
+  while (!file.eof())
+  {
+    file.read(reinterpret_cast<char *>(&blank_job), sizeof(Job));
     blank_job.idle_time = 0;
 
     file_queue[index] = blank_job;
@@ -72,31 +103,35 @@ int main() {
 
   size_t time = 0;
 
-  Node* priority_q = new Node;
+  Node *priority_q = new Node;
   priority_q->data = blank_job;
   priority_q->next = nullptr;
   priority_q->prev = nullptr;
 
-  Node* regular_q = new Node;
+  Node *regular_q = new Node;
   regular_q->data = blank_job;
   regular_q->next = nullptr;
   regular_q->prev = nullptr;
 
-  Node* idle_q = new Node;
+  Node *idle_q = new Node;
   idle_q->data = blank_job;
   idle_q->next = nullptr;
   idle_q->prev = nullptr;
 
   struct CPU cpu = {blank_job, 0, 0, 0};
-  CPU cpus[1] = {cpu};
+  CPU cpus[numCPUs] = {cpu};
 
   // Start simulation for initial metrics
-  while (time < 550) {
-    for (size_t i = 0; i < 7000; ++i) {
-      if (file_queue[i].arrival_time > time) break;
+  while (time < 550)
+  {
+    for (size_t i = 0; i < 7000; ++i)
+    {
+      if (file_queue[i].arrival_time > time)
+        break;
 
-      if (file_queue[i].arrival_time == time) {
-        Node* new_node = new Node;
+      if (file_queue[i].arrival_time == time)
+      {
+        Node *new_node = new Node;
         new_node->data = file_queue[i];
         new_node->next = nullptr;
         new_node->prev = nullptr;
@@ -106,7 +141,8 @@ int main() {
       }
     }
 
-    for (CPU& cpu : cpus) {
+    for (CPU &cpu : cpus)
+    {
       // load cpu with job from the queues according to the spec design of the
       // project and then do the processing
     }
@@ -114,13 +150,19 @@ int main() {
     ++time;
   }
 
-  // Continue simulation for final metrics
-  while (550 <= time < 10000) {
-    for (size_t i = 0; i < 7000; ++i) {
-      if (file_queue[i].arrival_time > time) break;
 
-      if (file_queue[i].arrival_time == time) {
-        Node* new_node = new Node;
+
+      // Continue simulation for final metrics
+      while (549 < time < 10000)
+  {
+    for (size_t i = 0; i < 7000; ++i)
+    {
+      if (file_queue[i].arrival_time > time)
+        break;
+
+      if (file_queue[i].arrival_time == time)
+      {
+        Node *new_node = new Node;
         new_node->data = file_queue[i];
         new_node->next = nullptr;
         new_node->prev = nullptr;
@@ -130,7 +172,8 @@ int main() {
       }
     }
 
-    for (CPU& cpu : cpus) {
+    for (CPU &cpu : cpus)
+    {
       // load cpu with job from the queues according to the spec design of the
       // project and then do the processing
     }
