5baaab3e2a38ab696a16462f30bc421d8479d86f
Kieran Llarena <baymax709@gmail.com>
2024-04-16 11:37:00 -0400

    fix: better arrival time data

--- program1.cpp
+++ program1.cpp
@@ -69,7 +69,7 @@ int main() {
   for (size_t i = 0; i < MAX_A_JOBS; ++i) {
     struct Job new_job;
     new_job.job_type = 'A';
-    new_job.arrival_time = get_random_int(6, 8);
+    new_job.arrival_time = i + get_random_int(6, 8);
     new_job.processing_time = get_random_int(1, 5);
 
     insert_node(head, new_job);
@@ -78,7 +78,7 @@ int main() {
   for (size_t i = 0; i < MAX_B_JOBS; ++i) {
     struct Job new_job;
     new_job.job_type = 'B';
-    new_job.arrival_time = get_random_int(2, 8);
+    new_job.arrival_time = MAX_A_JOBS + i + get_random_int(2, 8);
     new_job.processing_time = get_random_int(3, 13);
 
     insert_node(head, new_job);
@@ -87,7 +87,7 @@ int main() {
   for (size_t i = 0; i < MAX_C_JOBS; ++i) {
     struct Job new_job;
     new_job.job_type = 'C';
-    new_job.arrival_time = get_random_int(4, 18);
+    new_job.arrival_time = MAX_A_JOBS + MAX_B_JOBS + i + get_random_int(4, 18);
     new_job.processing_time = get_random_int(6, 12);
 
     insert_node(head, new_job);
@@ -96,13 +96,16 @@ int main() {
   for (size_t i = 0; i < MAX_D_JOBS; ++i) {
     struct Job new_job;
     new_job.job_type = 'D';
-    new_job.arrival_time = get_random_int(2, 12);
+    new_job.arrival_time =
+        MAX_A_JOBS + MAX_B_JOBS + MAX_C_JOBS + i + get_random_int(2, 12);
     new_job.processing_time = get_random_int(2, 22);
 
     insert_node(head, new_job);
   }
 
   // Overwriting all the data to try and make it more random
+  // Change the extension of this file to a .txt if you want to view it in text
+  // form
   fstream file("jobs.dat", ios::out | ios::in | ios::trunc);
   if (!file.is_open()) {
     cout << "Error opening file\n";
@@ -111,6 +114,10 @@ int main() {
 
   Node* current = head->next;
   while (current != nullptr) {
+    // Uncomment this code if you want to write this data to a .txt file
+    // file << current->data.job_type << " " << current->data.arrival_time << "
+    // "
+    //      << current->data.processing_time << "\n";
     file.write(reinterpret_cast<char*>(&current->data), sizeof(Job));
     current = current->next;
   }
